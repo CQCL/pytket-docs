e3bd7778af4b86c02c9cc228a3876e63691e63cc
seyon <seyon.sivarajah@cambridgequantum.com>
2019-06-14 15:44:50 +0100

    excluding backends

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'backends.rst', 'projectq_backend.rst', 'ibm_backend.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
