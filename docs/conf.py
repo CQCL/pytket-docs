12a7804381e399a9b3b850b2ce45dcacbbec85f8
seyon <seyon.sivarajah@cambridgequantum.com>
2019-06-19 09:42:05 +0100

    update docs version

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Cambridge Quantum Computing Ltd'
 author = 'Cambridge Quantum Computing Ltd'
 
 # The short X.Y version
-version = '0.2.0'
+version = '0.2.2'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
