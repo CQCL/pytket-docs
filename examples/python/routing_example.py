648db3b9d6180b9ca9a63c2d2ecf5ea6e7eba44a
Luca Mondada <luca.mondada@cambridgequantum.com>
2021-04-15 10:58:21 +0100

    Removed references to deleted architectures

--- examples/python/routing_example.py
+++ examples/python/routing_example.py
@@ -72,21 +72,9 @@ from pytket.routing import SquareGrid
 alternative_cube_architecture = SquareGrid(2, 2, 2)
 draw_graph(alternative_cube_architecture.coupling)
 
-# Other Architecture generators are supported including the HexagonalGrid:
-
-from pytket.routing import HexagonalGrid
-
-hex_architecture = HexagonalGrid(1, 2, 1)
-print(hex_architecture)
-draw_graph(hex_architecture.coupling)
-
-# and the CyclicButterfly:
-
-from pytket.routing import CyclicButterfly
-
-cb_architecture = CyclicButterfly(3)
-print(cb_architecture)
-draw_graph(cb_architecture.coupling)
+# In many cases, we are interested in the architectures of real devices.
+# These are available directly from the device backends, available within tket's respective extension packages.
+# We will see more on this further below.
 
 # In reality a Quantum Device has much more information to it than just its connectivity constraints. The Device class  encapsulates basic additional information used in fidelity aware qubit mapping methods available in pytket: gate errors and readout errors each qubit. Some methods in pytket can use this information to improve circuit performance when running on real hardware.
 #
