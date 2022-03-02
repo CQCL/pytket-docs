b6dac3182bb30eb66323dd0aefdd92266d498057
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2022-03-02 10:20:22 +0100

    Update examples/python/mapping_example.py
    Co-authored-by: Alec Edgington <54802828+cqc-alec@users.noreply.github.com>

--- examples/python/mapping_example.py
+++ examples/python/mapping_example.py
@@ -500,4 +500,4 @@ mm.route_circuit(
 )
 display.render_circuit_jupyter(c)
 
-# In this case the order of the method is not that relevant, because in each step of the routing only one of the methods is suitable anyway. In the first part of the circuit the mapping is done without inserting swaps by the aas method, for the second part lexi is adding one swap gate to the circuit.
+# In this case the order of the methods is not very relevant, because in each step of the routing only one of the methods is suitable. In the first part of the circuit the mapping is done without inserting swaps by the AAS method; in the second part one swap gate is added to the circuit.
