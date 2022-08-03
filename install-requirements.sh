c21c875dc0f0e99ea0f9ba1c726a52bdd631e732
melf <cqc@melf.de>
2022-08-03 09:18:01 +0100

    add install script and run checks on CI

---
+++ install-requirements.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# if version are updated, update the version in the requirements file 
+# and the check-examples.yml, too.
+# this duplication is currently necessary because of an issue with the pyjwt
+# version required from qcs-api-client < 0.21.0
+python -m pip install pytket==1.4.3
+python -m pip install pytket-pyquil==0.25.0
+python -m pip install qcs-api-client==0.21.0
+python -m pip install pytket-cirq==0.25.0
+python -m pip install pytket-projectq==0.23.0
+python -m pip install pytket-qiskit==0.27.0
+python -m pip install pytket-qsharp==0.27.0
+python -m pip install pytket-quantinuum==0.6.0
+python -m pip install pytket-qulacs==0.21.0
