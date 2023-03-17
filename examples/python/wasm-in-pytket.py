e3b12c4156f342c4a5b2c889647e89668f796f95
melf <cqc@melf.de>
2023-03-17 11:15:04 +0000

    fix black format

--- examples/python/wasm-in-pytket.py
+++ examples/python/wasm-in-pytket.py
@@ -18,40 +18,43 @@ print(repr(wfh))
 
 c = Circuit(0, 8)
 
-c.add_wasm("add_one",        # name of the function
-           wfh,              # wasm file handler
-           [1],              # number of bits in each of the parameter i32
-           [1],              # number of bits in each of the result i32
-           [Bit(0), Bit(1)]) # list of bits where the wasm op will be added to
+c.add_wasm(
+    "add_one",  # name of the function
+    wfh,  # wasm file handler
+    [1],  # number of bits in each of the parameter i32
+    [1],  # number of bits in each of the result i32
+    [Bit(0), Bit(1)],
+)  # list of bits where the wasm op will be added to
 
 # If you want to have more than one bit per parameter, you can add that in the following way. This will add the function "add_one" to read in for the first parameter from Bit(0) and Bit(1) and write the result to Bit(2), Bit(3) and Bit(4).
 
-c.add_wasm("add_one",        # name of the function
-           wfh,              # wasm file handler
-           [2],              # number of bits in each of the parameter i32
-           [3],              # number of bits in each of the result i32
-           [Bit(0), Bit(1),
-            Bit(2), Bit(3), Bit(4)]) # list of bits where the wasm op will be added to
+c.add_wasm(
+    "add_one",  # name of the function
+    wfh,  # wasm file handler
+    [2],  # number of bits in each of the parameter i32
+    [3],  # number of bits in each of the result i32
+    [Bit(0), Bit(1), Bit(2), Bit(3), Bit(4)],
+)  # list of bits where the wasm op will be added to
 
 # When adding functions with multible parameters this can be done in the same way:
 
-c.add_wasm("multi",          # name of the function
-           wfh,              # wasm file handler
-           [2, 1],           # number of bits in each of the parameter i32
-           [3],              # number of bits in each of the result i32
-           [Bit(0), Bit(1),  
-            Bit(5),              
-            Bit(2), Bit(3), Bit(4)]) # list of bits where the wasm op will be added to
+c.add_wasm(
+    "multi",  # name of the function
+    wfh,  # wasm file handler
+    [2, 1],  # number of bits in each of the parameter i32
+    [3],  # number of bits in each of the result i32
+    [Bit(0), Bit(1), Bit(5), Bit(2), Bit(3), Bit(4)],
+)  # list of bits where the wasm op will be added to
 
 # If you want to add two parameters with the same bits, that is fine, too.
 
-c.add_wasm("multi",          # name of the function
-           wfh,              # wasm file handler
-           [2, 2],           # number of bits in each of the parameter i32
-           [3],              # number of bits in each of the result i32
-           [Bit(0), Bit(1),
-            Bit(0), Bit(1),
-            Bit(2), Bit(3), Bit(4)]) # list of bits where the wasm op will be added to
+c.add_wasm(
+    "multi",  # name of the function
+    wfh,  # wasm file handler
+    [2, 2],  # number of bits in each of the parameter i32
+    [3],  # number of bits in each of the result i32
+    [Bit(0), Bit(1), Bit(0), Bit(1), Bit(2), Bit(3), Bit(4)],
+)  # list of bits where the wasm op will be added to
 
 # If you are working with registers in your circuit to organise the classical bits you can add wasm to your circuit using given registers for each parameter and the return value.
 
@@ -61,14 +64,18 @@ c0 = c.add_c_register("c0", 3)
 c1 = c.add_c_register("c1", 4)
 c2 = c.add_c_register("c2", 5)
 
-c.add_wasm_to_reg("multi",   # function name
-                  wfh,       # wasm file handler
-                  [c0, c1],  # register for each input parameter
-                  [c2])      # register for the result
-c.add_wasm_to_reg("add_one", # function name
-                  wfh,       # wasm file handler 
-                  [c2],      # register for each input parameter
-                  [c2])      # register for the result
+c.add_wasm_to_reg(
+    "multi",  # function name
+    wfh,  # wasm file handler
+    [c0, c1],  # register for each input parameter
+    [c2],
+)  # register for the result
+c.add_wasm_to_reg(
+    "add_one",  # function name
+    wfh,  # wasm file handler
+    [c2],  # register for each input parameter
+    [c2],
+)  # register for the result
 
 # The WASM might have some global data stored. To make sure this data is not messed up by function calls in the wrong order pytket will make sure that the order of the wasm calls within a circuit is not restructured. For this purpose pytket will add all wasm operation to a wasm_wire by default. If you are not worried about a possible restructure of the wasm calls in your circuit you have the option to not add the wasm_wire to your wasm operations. If you only want to stop some special reordering for some of your wasm operations you can add some the wasm operations to multiple wasm_wire to allow the restructuring in the intended way. Even if there are not wasm_wire given, pytket will only restructure the the wasm operations if there are no dependencies to in parameters or the results.
 
@@ -89,9 +96,10 @@ c.add_wasm("add_two", wfh, [1], [1], [Bit(4), Bit(4)], [])
 for g in c:
     print(g)
 
-# One helpful feature might be to plot the DAG of the circuit to get an overview of the different 
+# One helpful feature might be to plot the DAG of the circuit to get an overview of the different
 
 from pytket.utils import Graph
+
 g = Graph(c)
 g.view_DAG()
 
@@ -99,6 +107,7 @@ g.view_DAG()
 # In the last step we want to send the circuit with the wasm to a backend. First we create the backend. For this step you will need Quantinuum credentials.
 
 from pytket.extensions.quantinuum import QuantinuumBackend
+
 machine = "H1-1E"
 b = QuantinuumBackend(device_name=machine)
 b.login()
@@ -125,4 +134,3 @@ print(result)
 
 for shot in result.get_shots():
     print(shot)
-
