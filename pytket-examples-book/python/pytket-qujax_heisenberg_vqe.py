e9fd4b67c6ad1556d711c3d55d1982cda5457407
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-08-14 12:34:40 +0100

    move all notebook files into book directory

---
+++ pytket-examples-book/python/pytket-qujax_heisenberg_vqe.py
@@ -0,0 +1,180 @@
+from pytket import Circuit
+from pytket.circuit.display import render_circuit_jupyter
+from jax import numpy as jnp, random, vmap, grad, value_and_grad, jit
+import matplotlib.pyplot as plt
+
+import qujax
+from pytket.extensions.qujax import tk_to_qujax
+
+# # Let's start with a tket circuit
+# We place barriers to stop tket automatically rearranging gates and we also store the number of circuit parameters as we'll need this later.
+
+
+def get_circuit(n_qubits, depth):
+    n_params = 2 * n_qubits * (depth + 1)
+
+    param = jnp.zeros((n_params,))
+
+    circuit = Circuit(n_qubits)
+
+    k = 0
+    for i in range(n_qubits):
+        circuit.H(i)
+    for i in range(n_qubits):
+        circuit.Rx(param[k], i)
+        k += 1
+    for i in range(n_qubits):
+        circuit.Ry(param[k], i)
+        k += 1
+
+    for _ in range(depth):
+        for i in range(0, n_qubits - 1):
+            circuit.CZ(i, i + 1)
+        circuit.add_barrier(range(0, n_qubits))
+        for i in range(n_qubits):
+            circuit.Rx(param[k], i)
+            k += 1
+        for i in range(n_qubits):
+            circuit.Ry(param[k], i)
+            k += 1
+    return circuit, n_params
+
+
+n_qubits = 4
+depth = 2
+circuit, n_params = get_circuit(n_qubits, depth)
+render_circuit_jupyter(circuit)
+
+# # Now let's invoke qujax
+# The `pytket.extensions.qujax.tk_to_qujax` function will generate a parameters -> statetensor function for us.
+
+param_to_st = tk_to_qujax(circuit)
+
+# Let's try it out on some random parameters values. Be aware that's JAX's random number generator requires a `jax.random.PRNGkey` every time it's called - more info on that [here](https://jax.readthedocs.io/en/latest/jax.random.html).
+# Be aware that we still have convention where parameters are specified as multiples of $\pi$ - that is in [0,2].
+
+params = random.uniform(random.PRNGKey(0), shape=(n_params,), minval=0.0, maxval=2.0)
+statetensor = param_to_st(params)
+print(statetensor)
+print(statetensor.shape)
+
+# Note that this function also has an optional second argument where an initiating `statetensor_in` can be provided. If it is not provided it will default to the all 0s state (as we use here).
+
+# We can obtain statevector by simply calling `.flatten()`
+
+statevector = statetensor.flatten()
+statevector.shape
+
+# And sampling probabilities by squaring the absolute value of the statevector
+
+sample_probs = jnp.square(jnp.abs(statevector))
+plt.bar(jnp.arange(statevector.size), sample_probs)
+
+# # Cost function
+
+# Now we have our `param_to_st` function we are free to define a cost function that acts on bitstrings (e.g. maxcut) or integers by directly wrapping a function around `param_to_st`. However, cost functions defined via quantum Hamiltonians are a bit more involved.
+# Fortunately, we can encode an Hamiltonian in JAX via the `qujax.get_statetensor_to_expectation_func` function which generates a statetensor -> expected value function for us.
+# It takes three arguments as input
+# - `gate_seq_seq`: A list of string (or array) lists encoding the gates in each term of the Hamiltonian. I.e. `[['X','X'], ['Y','Y'], ['Z','Z']]` corresponds to $H = aX_iX_j +  bY_kY_l + cZ_mZ_n$ with qubit indices $i,j,k,l,m,n$ specified in the second argument and coefficients $a,b,c$ specified in the third argument
+# - `qubit_inds_seq`: A list of integer lists encoding which qubit indices to apply the aforementioned gates. I.e. `[[0, 1],[0,1],[0,1]]`. Must have the same structure as `gate_seq_seq` above.
+# - `coefficients`: A list of floats encoding any coefficients in the Hamiltonian. I.e. `[2.3, 0.8, 1.2]` corresponds to $a=2.3,b=0.8,c=1.2$ above. Must have the same length as the two above arguments.
+
+# More specifically let's consider the problem of finding the ground state of the quantum Heisenberg Hamiltonian
+# $$ H = \sum_{i=1}^{n_\text{qubits}-1} X_i X_{i+1} + Y_i Y_{i+1} + Z_i Z_{i+1}. $$
+# As described, we define the Hamiltonian via its gate strings, qubit indices and coefficients.
+
+hamiltonian_gates = [["X", "X"], ["Y", "Y"], ["Z", "Z"]] * (n_qubits - 1)
+hamiltonian_qubit_inds = [
+    [int(i), int(i) + 1] for i in jnp.repeat(jnp.arange(n_qubits), 3)
+]
+coefficients = [1.0] * len(hamiltonian_qubit_inds)
+
+print("Gates:\t", hamiltonian_gates)
+print("Qubits:\t", hamiltonian_qubit_inds)
+print("Coefficients:\t", coefficients)
+
+# Now let's get the Hamiltonian as a pure JAX function
+
+st_to_expectation = qujax.get_statetensor_to_expectation_func(
+    hamiltonian_gates, hamiltonian_qubit_inds, coefficients
+)
+
+# Let's check it works on the statetensor we've already generated.
+
+expected_val = st_to_expectation(statetensor)
+expected_val
+
+# Now let's wrap the `param_to_st` and `st_to_expectation` together to give us an all in one `param_to_expectation` cost function.
+
+param_to_expectation = lambda param: st_to_expectation(param_to_st(param))
+
+param_to_expectation(params)
+
+# Sanity check that a different, randomly generated set of parameters gives us a new expected value.
+
+new_params = random.uniform(
+    random.PRNGKey(1), shape=(n_params,), minval=0.0, maxval=2.0
+)
+param_to_expectation(new_params)
+
+# # We can now use autodiff for fast, exact gradients within a VQE algorithm
+# The `param_to_expectation` function we created is a pure JAX function and outputs a scalar. This means we can pass it to `jax.grad` (or even better `jax.value_and_grad`).
+
+cost_and_grad = value_and_grad(param_to_expectation)
+
+# The `cost_and_grad` function returns a tuple with the exact cost value and exact gradient evaluated at the parameters.
+
+cost_and_grad(params)
+
+# # Now we have all the tools we need to design our VQE!
+# We'll just use vanilla gradient descent with a constant stepsize
+
+
+def vqe(init_param, n_steps, stepsize):
+    params = jnp.zeros((n_steps, n_params))
+    params = params.at[0].set(init_param)
+
+    cost_vals = jnp.zeros(n_steps)
+    cost_vals = cost_vals.at[0].set(param_to_expectation(init_param))
+
+    for step in range(1, n_steps):
+        cost_val, cost_grad = cost_and_grad(params[step - 1])
+        cost_vals = cost_vals.at[step].set(cost_val)
+
+        new_param = params[step - 1] - stepsize * cost_grad
+        params = params.at[step].set(new_param)
+
+        print("Iteration:", step, "\tCost:", cost_val, end="\r")
+    print("\n")
+    return params, cost_vals
+
+
+# Ok enough talking, let's run (and whilst we're at it we'll time it too)
+
+# %time
+vqe_params, vqe_cost_vals = vqe(params, n_steps=250, stepsize=0.01)
+
+# Let's plot the results...
+
+plt.plot(vqe_cost_vals)
+plt.xlabel("Iteration")
+plt.ylabel("Cost")
+
+# Pretty good!
+
+# # `jax.jit` speedup
+# One last thing... We can significantly speed up the VQE above via the `jax.jit`. In our current implementation, the expensive `cost_and_grad` function is compiled to [XLA](https://www.tensorflow.org/xla)  and then executed at each call. By invoking `jax.jit` we ensure that the function is compiled only once (on the first call) and then simply executed at each future call - this is much faster!
+
+cost_and_grad = jit(cost_and_grad)
+
+# We'll demonstrate this using the second set of initial parameters we randomly generated (to be sure of no caching).
+
+# %time
+new_vqe_params, new_vqe_cost_vals = vqe(new_params, n_steps=250, stepsize=0.01)
+
+# That's some speedup!
+# But let's also plot the training to be sure it converged correctly
+
+plt.plot(new_vqe_cost_vals)
+plt.xlabel("Iteration")
+plt.ylabel("Cost")
---
+++ pytket-examples-book/python/pytket-qujax_qaoa.py
@@ -0,0 +1,134 @@
+# # Symbolic circuits with `qujax` and `pytket-qujax`
+# In this notebook we will show how to manipulate symbolic circuits with the `pytket-qujax` extension. In particular, we will consider a QAOA and an Ising Hamiltonian.
+
+from pytket import Circuit
+from pytket.circuit.display import render_circuit_jupyter
+from jax import numpy as jnp, random, value_and_grad, jit
+from sympy import Symbol
+import matplotlib.pyplot as plt
+
+import qujax
+from pytket.extensions.qujax import tk_to_qujax
+
+# # QAOA
+# The Quantum Approximate Optimization Algorithm (QAOA), first introduced by [Farhi et al.](https://arxiv.org/pdf/1411.4028.pdf), is a quantum variational algorithm used to solve optimization problems. It consists of a unitary $U(\beta, \gamma)$ formed by alternate repetitions of $U(\beta)=e^{-i\beta H_B}$ and $U(\gamma)=e^{-i\gamma H_P}$, where $H_B$ is the mixing Hamiltonian and $H_P$ the problem Hamiltonian. The goal is to find the optimal parameters that minimize $H_P$.
+# Given a depth $d$, the expression of the final unitary is $U(\beta, \gamma) = U(\beta_d)U(\gamma_d)\cdots U(\beta_1)U(\gamma_1)$. Notice that for each repetition the parameters are different.
+# ## Problem Hamiltonian
+# QAOA uses a problem dependent ansatz. Therefore, we first need to know the problem that we want to solve. In this case we will consider an Ising Hamiltonian with only $Z$ interactions. Given a set of pairs (or qubit indices) $E$, the problem Hamiltonian will be:
+# $$H_P = \sum_{(i, j) \in E}\alpha_{ij}Z_iZ_j,$$
+# where $\alpha_{ij}$ are the coefficients.
+# Let's build our problem Hamiltonian with random coefficients and a set of pairs for a given number of qubits:
+
+n_qubits = 4
+hamiltonian_qubit_inds = [(0, 1), (1, 2), (0, 2), (1, 3)]
+hamiltonian_gates = [["Z", "Z"]] * (len(hamiltonian_qubit_inds))
+
+# Notice that in order to use the random package from jax we first need to define a seeded key
+
+seed = 13
+key = random.PRNGKey(seed)
+coefficients = random.uniform(key, shape=(len(hamiltonian_qubit_inds),))
+
+print("Gates:\t", hamiltonian_gates)
+print("Qubits:\t", hamiltonian_qubit_inds)
+print("Coefficients:\t", coefficients)
+
+# ## Variational Circuit
+# Before constructing the circuit, we still need to select the mixing Hamiltonian. In our case, we will be using $X$ gates in each qubit, so $H_B = \sum_{i=1}^{n}X_i$, where $n$ is the number of qubits. Notice that the unitary $U(\beta)$, given this mixing Hamiltonian, is an $X$ rotation in each qubit with angle $\beta$.
+# As for the unitary corresponding to the problem Hamiltonian, $U(\gamma)$, it has the following form:
+# $$U(\gamma)=\prod_{(i, j) \in E}e^{-i\gamma\alpha_{ij}Z_iZ_j}$$
+# The operation $e^{-i\gamma\alpha_{ij}Z_iZ_j}$ can be performed using two CNOT gates with qubit $i$ as control and qubit $j$ as target and a $Z$ rotation in qubit $j$ in between them, with angle $\gamma\alpha_{ij}$.
+# Finally, the initial state used, in general, with the QAOA is an equal superposition of all the basis states. This can be achieved adding a first layer of Hadamard gates in each qubit at the beginning of the circuit.
+
+# With all the building blocks, let's construct the symbolic circuit using tket. Notice that in order to define the parameters, we use the ```Symbol``` object from the `sympy` package. More info can be found in this [documentation](https://cqcl.github.io/pytket/manual/manual_circuit.html#symbolic-circuits). In order to later convert the circuit to qujax, we need to return the list of symbolic parameters as well.
+
+
+def qaoa_circuit(n_qubits, depth):
+    circuit = Circuit(n_qubits)
+    p_keys = []
+
+    # Initial State
+    for i in range(n_qubits):
+        circuit.H(i)
+
+    for d in range(depth):
+        # Hamiltonian unitary
+        gamma_d = Symbol(f"γ_{d}")
+        for index in range(len(hamiltonian_qubit_inds)):
+            pair = hamiltonian_qubit_inds[index]
+            coef = coefficients[index]
+
+            circuit.CX(pair[0], pair[1])
+            circuit.Rz(gamma_d * coef, pair[1])
+            circuit.CX(pair[0], pair[1])
+            circuit.add_barrier(range(0, n_qubits))
+        p_keys.append(gamma_d)
+
+        # Mixing unitary
+        beta_d = Symbol(f"β_{d}")
+        for i in range(n_qubits):
+            circuit.Rx(beta_d, i)
+        p_keys.append(beta_d)
+
+    return circuit, p_keys
+
+
+depth = 3
+circuit, keys = qaoa_circuit(n_qubits, depth)
+
+keys
+
+# Let's check the circuit:
+
+render_circuit_jupyter(circuit)
+
+# # Now for `qujax`
+# The `pytket.extensions.qujax.tk_to_qujax` function will generate a parameters -> statetensor function for us. However, in order to convert a symbolic circuit we first need to define the `symbol_map`. This object maps each symbol key to their corresponding index. In our case, since the object `keys` contains the symbols in the correct order, we can simply construct the dictionary as follows:
+
+symbol_map = {keys[i]: i for i in range(len(keys))}
+
+symbol_map
+
+# Then, we invoke the `tk_to_qujax` with both the circuit and the symbolic map.
+
+param_to_st = tk_to_qujax(circuit, symbol_map=symbol_map)
+
+# And we also construct the expectation map using the problem Hamiltonian via qujax:
+
+st_to_expectation = qujax.get_statetensor_to_expectation_func(
+    hamiltonian_gates, hamiltonian_qubit_inds, coefficients
+)
+
+param_to_expectation = lambda param: st_to_expectation(param_to_st(param))
+
+# # Training process
+# We construct a function that, given a parameter vector, returns the value of the cost function and the gradient.
+# We also `jit` to avoid recompilation, this means that the expensive `cost_and_grad` function is compiled once into a very fast XLA (C++) function which is then executed at each iteration. Alternatively, we could get the same speedup by replacing our `for` loop with `jax.lax.scan`. You can read more about JIT compilation in the [JAX documentation](https://jax.readthedocs.io/en/latest/jax-101/02-jitting.html).
+
+cost_and_grad = jit(value_and_grad(param_to_expectation))
+
+# For the training process we'll use vanilla gradient descent with a constant stepsize:
+
+seed = 123
+key = random.PRNGKey(seed)
+init_param = random.uniform(key, shape=(len(symbol_map),))
+
+n_steps = 150
+stepsize = 0.01
+
+param = init_param
+
+cost_vals = jnp.zeros(n_steps)
+cost_vals = cost_vals.at[0].set(param_to_expectation(init_param))
+
+for step in range(1, n_steps):
+    cost_val, cost_grad = cost_and_grad(param)
+    cost_vals = cost_vals.at[step].set(cost_val)
+    param = param - stepsize * cost_grad
+    print("Iteration:", step, "\tCost:", cost_val, end="\r")
+
+# Let's visualise the gradient descent
+
+plt.plot(cost_vals)
+plt.xlabel("Iteration")
+plt.ylabel("Cost")
