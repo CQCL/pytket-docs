3aa1ebf7a19d6cc102e9859876f45dc8562ff85c
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-11-21 09:43:13 +0000

    Use m for the number of measurement qubits

--- examples/python/phase_estimation.py
+++ examples/python/phase_estimation.py
@@ -44,7 +44,7 @@
 #
 # $$
 # \begin{equation}
-# QFT : |j\rangle\ \longmapsto \frac{1}{\sqrt{N}} \sum_{k=0}^{N - 1} e^{2 \pi ijk/N}|k\rangle, \quad N= 2^k
+# QFT : |j\rangle\ \longmapsto \frac{1}{\sqrt{N}} \sum_{k=0}^{N - 1} e^{2 \pi ijk/N}|k\rangle, \quad N= 2^m
 # \end{equation}
 # $$
 #
@@ -125,7 +125,7 @@ render_circuit_jupyter(qft_circ)
 #
 # $$
 # \begin{equation}
-# \text{QFT}^† : \frac{1}{\sqrt{N}} \sum_{k=0}^{N - 1} e^{2 \pi ijk/N}|k\rangle \longmapsto |j\rangle\,, \quad N= 2^k
+# \text{QFT}^† : \frac{1}{\sqrt{N}} \sum_{k=0}^{N - 1} e^{2 \pi ijk/N}|k\rangle \longmapsto |j\rangle\,, \quad N= 2^m
 # \end{equation}
 # $$
 #
