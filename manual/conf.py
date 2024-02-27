ba21ec1bf7d8234da69aec1333b17c40d2a7e264
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-02-27 16:13:04 +0000

    configure myst-parser

--- manual/conf.py
+++ manual/conf.py
@@ -70,3 +70,17 @@ blog_feed_archives = True
 blog_feed_fulltext = True
 
 blog_post_pattern = "blog/*/*"
+
+# List of Sphinx extensions used
+extensions = ["ablog", "myst_parser", "sphinx_copybutton", "sphinx_favicon"]
+
+# ----- MyST parser config -----
+
+myst_enable_extensions = ["dollarmath", "html_image", "attrs_inline"]
+
+myst_update_mathjax = False
+
+suppress_warnings = ["myst.xref_missing"]
+
+# Exclude README from blog build
+exclude_patterns = ["README.md"]
