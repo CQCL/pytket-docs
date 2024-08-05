abcfe33bd15e18e9ea0f79eb28543d1e48afbf5b
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-08-05 12:23:35 +0100

    don't excute flaky qujax example

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,7 @@ nb_execution_excludepatterns = [
     "examples/backends/comparing_simulators.ipynb",
     "examples/algorithms_and_protocols/expectation_value_example.ipynb",
     "examples/algorithms_and_protocols/pytket-qujax_heisenberg_vqe.ipynb",
+    "examples/algorithms_and_protocols/pytket-qujax-classification.ipynb",
     "examples/algorithms_and_protocols/spam_example.ipynb",
     "examples/algorithms_and_protocols/entanglement_swapping.ipynb",
 ]
