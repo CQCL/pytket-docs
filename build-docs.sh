fd7bf075daff194cf9a6be49590432822829007f
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-10-07 12:01:29 +0100

    update build-docs script

--- build-docs.sh
+++ build-docs.sh
@@ -3,5 +3,15 @@
 cd docs/
 
 rm -rf build/
-sphinx-build -b html . build -W
+
+cp -R pytket-docs-theming/_static .
+cp -R pytket-docs-theming/quantinuum-sphinx .
+cp pytket-docs-theming/conf.py .
+
+sphinx-build -b html . build -D html_title="pytket user guide"
+
+# Remove copied files. This ensures reusability.
+rm -r _static 
+rm -r quantinuum-sphinx
+rm conf.py
 
